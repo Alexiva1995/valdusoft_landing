e107b21c9e3d1d800f91103884bebdf8d60457e2
ChrisN01 <crisleivysngil@gmail.com>
2020-11-16 19:12:58 -0430

    Filtrar proyectos por tag en la seccion de portafolio

--- resources/views/landing/componentes/nuestrosTrabajos.blade.php
+++ resources/views/landing/componentes/nuestrosTrabajos.blade.php
@@ -8,14 +8,14 @@
                     <h2><strong>Este es nuestro trabajo</strong></h2>
                 </div>
                 <div class="right menu">
-                        <a class="item">
-                                <span>Todos</span>
-                        </a>
-                    @foreach ($tags as $tag)
-                        <a class="item">
-                            <span>{{$tag->name}}</span>
-                        </a>
-                    @endforeach
+                <!--TABS-->
+                <div class="ui top attached tabular menu">
+                <a class="item active" data-tab="todos">Todos</a>
+                @foreach ($tags as $tag)
+                    <a class="item" data-tab="item-{{$tag->id}}">{{$tag->name}}</a>
+                @endforeach
+                </div>
+                <!--End Tabs-->
                 </div>
             </div>
         </div>
@@ -25,19 +25,21 @@
                 <h2><strong>Este es nuestro trabajo</strong></h2>
                 <i class="dropdown icon"></i>
                 <div class="menu ">
-                        <a class="item text-center">
-                                <span>Todos</span>
-                        </a>
-                    @foreach ($tags as $tag)
-                        <a class="item text-center">
-                            <span>{{$tag->name}}</span>
-                        </a>
-                    @endforeach
+                <!--TABS-->
+                <div class="ui top attached tabular menu">
+                <a class="item active text-center" data-tab="todos">Todos</a>
+                @foreach ($tags as $tag)
+                    <a class="item text-center" data-tab="item-{{$tag->id}}">{{$tag->name}}</a>
+                @endforeach
+                </div>
+                <!--End Tabs-->
                 </div>
             </div>
         </div>
     </div>
-    <div class="ui grid">
+ <!--Content tab-->
+    <div class="ui bottom attached tab segment active" data-tab="todos">
+     <div class="ui grid">
         <div class="doubling four column row portafolio">
             @foreach($projects as $project)
             <div class="column">
@@ -60,9 +62,97 @@
             </div>
             @endforeach
           </div>
-          <div class="ui center aligned container">
-                <button class="large ui inverted blue button">Ver más</button>
-          </div>
-
     </div>
+    </div>
+    @foreach ($tags as $tag)
+        <div class="ui bottom attached tab segment" data-tab="item-{{$tag->id}}">
+              <div class="ui grid">
+                <div class="doubling four column row portafolio">
+                    @foreach($projects as $project)
+                    @if($project->tag->name == 'Apps' && $tag->name == 'Apps' )
+                         <div class="column">
+                        <!--<img src="{{asset('assets/img/sistema/Logo-1.png')}}" alt="">-->
+                        <div class="container card-works">
+                                <div class="overlay">
+                                <div class = "items"></div>
+                                <div class = "items company">
+                                        <p>VALDU<strong>SOFT</strong></p>
+                                    </div>
+                                <div class = "items head">
+                                    <p>{{$project->name}}</p>
+                                </div>
+                                <div class = "items category">
+                                        <p>{{$project->tag->name}}</p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    @endif
+                    @if($project->tag->name == 'Diseño Web' && $tag->name == 'Diseño Web' )
+                         <div class="column">
+                        <!--<img src="{{asset('assets/img/sistema/Logo-1.png')}}" alt="">-->
+                        <div class="container card-works">
+                                <div class="overlay">
+                                <div class = "items"></div>
+                                <div class = "items company">
+                                        <p>VALDU<strong>SOFT</strong></p>
+                                    </div>
+                                <div class = "items head">
+                                    <p>{{$project->name}}</p>
+                                </div>
+                                <div class = "items category">
+                                        <p>{{$project->tag->name}}</p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    @endif
+                     @if($project->tag->name == 'Desarrollo Web' && $tag->name == 'Desarrollo Web' )
+                         <div class="column">
+                        <!--<img src="{{asset('assets/img/sistema/Logo-1.png')}}" alt="">-->
+                        <div class="container card-works">
+                                <div class="overlay">
+                                <div class = "items"></div>
+                                <div class = "items company">
+                                        <p>VALDU<strong>SOFT</strong></p>
+                                    </div>
+                                <div class = "items head">
+                                    <p>{{$project->name}}</p>
+                                </div>
+                                <div class = "items category">
+                                        <p>{{$project->tag->name}}</p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    @endif
+                    @if($project->tag->name == 'Diseño gráfico' && $tag->name == 'Diseño gráfico' )
+                         <div class="column">
+                        <!--<img src="{{asset('assets/img/sistema/Logo-1.png')}}" alt="">-->
+                        <div class="container card-works">
+                                <div class="overlay">
+                                <div class = "items"></div>
+                                <div class = "items company">
+                                        <p>VALDU<strong>SOFT</strong></p>
+                                    </div>
+                                <div class = "items head">
+                                    <p>{{$project->name}}</p>
+                                </div>
+                                <div class = "items category">
+                                        <p>{{$project->tag->name}}</p>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    @endif
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    @endforeach
+    <!--EndContent tab-->
 </div>
