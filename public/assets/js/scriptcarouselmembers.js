ff184232c43198c07a7197c563498d3bf7d3779c
luisana <luisanaelenamarin@gmail.com>
2021-02-05 12:20:00 -0400

    carpeta publica

---
+++ public/assets/js/scriptcarouselmembers.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function($){$('.slider').slick({dots:!1,infinite:!0,speed:500,slidesToShow:4,slidesToScroll:1,autoplay:!0,autoplaySpeed:2000,arrows:!0,responsive:[{breakpoint:600,settings:{slidesToShow:2,slidesToScroll:1}},{breakpoint:400,settings:{arrows:!1,slidesToShow:1,slidesToScroll:1}}]})})
