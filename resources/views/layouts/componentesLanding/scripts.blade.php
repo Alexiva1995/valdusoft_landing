c7e8796c0b075b3ca05996904d77ec05bc6af254
luisana <luisanaelenamarin@gmail.com>
2021-02-10 10:33:24 -0400

    combinar archivos css y js para menos peticiones

--- resources/views/layouts/componentesLanding/scripts.blade.php
+++ resources/views/layouts/componentesLanding/scripts.blade.php
@@ -5,6 +5,15 @@
 <script src="{{asset('assets/Semantic/semantic.min.js')}}"></script>
 {{-- Slick --}}
 <script src="{{asset('assets/Slick/slick/slick.min.js')}}"></script>
-{{-- Custom JS --}}
+{{-- Custom JS 
 <script src="{{asset('assets/js/scriptLanding.js')}}"></script>
-<script src="{{asset('assets/js/scriptcarouselmembers.js')}}"></script>
+<script src="{{asset('assets/js/scriptcarouselmembers.js')}}"></script>--}}
+@php 
+	use MatthiasMullie\Minify;
+	$minifierJs = new Minify\JS('assets/js/scriptLanding.js');
+	$minifierJs->add('assets/js/scriptcarouselmembers.js');
+@endphp
+
+<script>
+	{!! $minifierJs->minify() !!}
+</script>
--- resources/views/layouts/componentesLanding/styles.blade.php
+++ resources/views/layouts/componentesLanding/styles.blade.php
@@ -1,12 +1,22 @@
 <link rel="shortcut icon" href="{{ asset('assets/img/sistema/faviconlogo.ico') }}">
 {{-- Font --}}
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@900&display=swap" rel="stylesheet"> 
-{{-- Semantic --}}
-<link rel="stylesheet" href="{{asset('assets/Semantic/semantic.min.css')}}">
-{{-- Slick --}}
-<link rel="stylesheet" href="{{asset('assets/Slick/slick/slick.css')}}">
 <link rel=preload href="{{asset('assets/fonts/icons.woff2')}}">
 <link rel=preload href="{{asset('assets/fonts/brand-icons.woff2')}}">
+{{-- Semantic --}}
+<link rel="stylesheet" href="{{asset('assets/Semantic/semantic.min.css')}}">
+{{-- Slick 
+<link rel="stylesheet" href="{{asset('assets/Slick/slick/slick.css')}}">
 <link rel="stylesheet" href="{{asset('assets/Slick/slick/slick-theme.css')}}">
-{{-- Css Custom Landing --}}
-<link rel="stylesheet" href="{{asset('assets/css/landingcss.css')}}">
+{{-- Css Custom Landing 
+<link rel="stylesheet" href="{{asset('assets/css/landingcss.css')}}">--}}
+@php 
+	use MatthiasMullie\Minify;
+	$minifier = new Minify\CSS('assets/Slick/slick/slick.css');
+	$minifier->add('assets/Slick/slick/slick-theme.css');
+	$minifier->add('assets/css/landingcss.css');
+@endphp
+
+<style>
+	{!! $minifier->minify() !!}
+</style>
